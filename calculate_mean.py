d227d70263ca86eaa6050300b46b8ea177bc9128
Leila Shams Ashkezari <l.shamsashkezari@tudelft.nl>
2024-02-21 15:09:30 +0100

    add docstring

--- calculate_mean.py
+++ calculate_mean.py
@@ -1,6 +1,10 @@
+
 import numpy as np
 
 def calculate_mean(data):
+"""this function compute the mean of an array.
+input: one dimensional array
+output: mean of an array """
 	mean = np.mean(data)
 	return mean
 
